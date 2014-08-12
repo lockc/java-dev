33c4b3ed1ca9f10122c8d383bbc8e18861e38fba
Chris Lock <riff.lock@gmail.com>
2014-08-12 11:04:19 +0100

    java cert

---
+++ java7-cert/src/main/java/sandbox/lockc/javacert/concurrency/ForkJoinFrameowrk.java
@@ -0,0 +1,59 @@
+package sandbox.lockc.javacert.concurrency;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ForkJoinFrameowrk {
+
+	public static void main(String[] args) {
+				
+		List<Integer> numbers = Arrays.asList(
+				1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21);
+		
+		SumTask task = new SumTask(numbers);
+		
+		ForkJoinPool pool = new ForkJoinPool(4);
+		Integer result = pool.invoke(task);
+		System.out.println(result);
+		
+	}
+	
+	
+	
+	
+	
+	@SuppressWarnings("serial")
+	public static class SumTask extends RecursiveTask<Integer> {
+
+		private List<Integer> numbers;
+				
+		public SumTask(List<Integer> numbers) {
+			this.numbers = numbers;
+		}
+		
+		@Override
+		public Integer compute() {
+			
+			if(numbers.size() <= 5) {
+				int sum = 0;
+				for(Integer number : numbers) {
+					sum += number;
+				}
+				return sum;
+			} else {
+				List<Integer> firstList = numbers.subList(0, (numbers.size() / 2));
+				List<Integer> secondList = numbers.subList(numbers.size() / 2, numbers.size());
+				
+				SumTask firstHalf = new SumTask(firstList);
+				firstHalf.fork();	
+				SumTask secondHalf = new SumTask(secondList);
+				return firstHalf.join() + secondHalf.compute(); 
+				
+			}
+		}
+		
+	}
+
+}
