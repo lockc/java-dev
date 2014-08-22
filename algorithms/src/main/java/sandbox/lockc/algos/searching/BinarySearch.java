c58b04dbcb2de7376da061ded2470fac4b7482b0
Chris Lock <riff.lock@gmail.com>
2014-08-22 17:46:04 +0100

    algos

--- algorithms/src/main/java/sandbox/lockc/algos/searching/BinarySearch.java
+++ algorithms/src/main/java/sandbox/lockc/algos/searching/BinarySearch.java
@@ -17,24 +17,20 @@ public class BinarySearch {
 		};
 		
 		System.out.println(binarySearch(numbers, 10));
-		
+		System.out.println(recursiveBinarySearch(numbers, 0, numbers.length - 1, 10));
 	}
 	
 	
 	public static int binarySearch(int[] numbers, int x) {
-		
 		int p = 0, r = numbers.length - 1, q = -1;
 		
 		while(p <= r) {
-			
 			q = (p + r) / 2;
-			
 			int number = numbers[q];
 			
 			// For illustration only
 			int[] sub = Arrays.copyOfRange(numbers, p, r + 1);
-			System.out.println(String.format("p=%d, q=%s, r=%d", p, q, r));
-			
+			System.out.println(String.format("p=%d, q=%s, r=%d, s=%s", p, q, r, Arrays.toString(sub)));
 			
 			if(number == x) {
 				return number;
@@ -42,11 +38,27 @@ public class BinarySearch {
 				r = q - 1;
 			} else {
 				p = q + 1;
-			}
-			
+			}	
 		}
 		
 		return -1;
+	}
+	
+	public static int recursiveBinarySearch(int[] numbers, int p, int r, int x) {
+		int q = (p + r) / 2;
+		int number = numbers[q];
+		
+		// For illustration only
+		int[] sub = Arrays.copyOfRange(numbers, p, r + 1);
+		System.out.println(String.format("p=%d, q=%s, r=%d, s=%s", p, q, r, Arrays.toString(sub)));
+				
+		if(number == x) {
+			return number;
+		} else if (number > x) {
+			return recursiveBinarySearch(numbers, p, q - 1, x);
+		} else {
+			return recursiveBinarySearch(numbers, q + 1, r, x);
+		}
 	}
 	
 	
---
+++ algorithms/src/main/java/sandbox/lockc/algos/sorting/SelectionSort.java
@@ -0,0 +1,56 @@
+package sandbox.lockc.algos.sorting;
+
+import java.util.Arrays;
+
+/**
+ * An example of a selection sort algorithm.
+ * 
+ *  @see Algorithms Unlocked by Thomas H. Corman. Page 32, Chapter 3.
+ * 
+ * @author lockc
+ *
+ */
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = new int[] {
+				2, 9, 88, 3, 3, 4, 7, 1, 5, 6, 7, 7, 8, 9, 10, 3
+		};
+		
+		System.out.println(Arrays.toString(selectionSort(numbers)));
+		
+	}
+
+	public static int[] selectionSort(int[] numbers) {
+		
+		for(int i = 0; i < numbers.length - 1; i++) {
+			
+			// find the smallest number in the sub array [i..length - 1]
+			int smallest = smallest(numbers, i);
+			
+			// swap the smallest number in the array element i
+			int temp = numbers[i];
+			numbers[i] = numbers[smallest];
+			numbers[smallest] = temp;
+			
+		}
+		
+		return numbers;
+	}
+	
+	
+	
+	private static int smallest(int[] numbers, int from) {
+		int smallest = from;
+		int last = numbers[from];
+		
+		for(int i = from + 1; i < numbers.length; i++) {
+			if(numbers[i] < last) {
+				smallest = i;
+				last = numbers[smallest];
+			}
+		}
+		return smallest;
+	}
+}
